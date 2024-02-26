5ff4361d3e3b263ca0ba8eb3a0d0a97238569c2e
Waqi <117337520+MohdWaqi@users.noreply.github.com>
2024-02-26 13:42:54 +0530

    routes procted

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Login from './Pages/Login'
 import Register from './Pages/Register'
 import SingleProduct from './Pages/SingleProduct'
 import Wishlist from './Pages/Wishlist'
+import PrivateRoute from './Private/PrivateRoute'
 
 function App() {
   return (
@@ -14,7 +15,9 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path="/products" element= {<AllProducts/>}/>
       <Route path="/register" element={<Register/>}/>
+      <Route element={<PrivateRoute/>}>
       <Route path="/wishlist" element={<Wishlist/>}/>
+      </Route>
       <Route path="/login" element={<Login/>}/>
       <Route path="/products/:id" element= {<SingleProduct/>}/>
       <Route path="/bag" element={<Bag/>}/>
---
+++ src/Private/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react'
+import { AuthContext } from '../Context/AuthContextProvider'
+import { Navigate, Outlet } from 'react-router-dom'
+
+const PrivateRoute = () => {
+    const {isAuth} = useContext(AuthContext)
+
+  return (
+    isAuth?<Outlet/>:<Navigate to="/login"/>
+  )
+}
+
+export default PrivateRoute
